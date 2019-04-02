dbc0c9fb70b062ed660b3bad81dfb2eef400c029
Oksana <okkaragicheva@gmail.com>
2019-04-02 12:03:44 +0300

    Gallery and AlbumsDropdown components were created

---
+++ src/Gallery.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AlbumsDropdown from '../src/AlbumsDropdown';
+
+class Gallery extends Component {
+  render() {
+    return (
+      <AlbumsDropdown />
+    );
+  }
+}
+
+export default Gallery;
