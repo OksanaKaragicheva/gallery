4ab4a0f0b119c3d9f961fe22dedd26e0eb8a9fb5
Oksana <okkaragicheva@gmail.com>
2019-04-03 00:06:23 +0300

    New component was created

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Photo from '../Components/Photo';
 
 function Photos(props) {
     return (
       <Container>
         <Row>
-          {props.photos.map((photo) => {
-            return (
-             <Col xs={6} md={4} key={photo.id}>
-              <figure key={photo.id}>
-                <Image src={photo.thumbnailUrl} rounded />
-                  <figcaption>
-                    {photo.title}
-                  </figcaption>
-              </figure>
-             </Col>
-
-        )
-      }
-      )
-
-      }
+          <Photo />
         </Row>
       </Container>
     );
