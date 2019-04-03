8c93e919cc9cccdeb2b75fa516fd09b631c82a21
Oksana <okkaragicheva@gmail.com>
2019-04-03 12:26:58 +0300

    Key atribute was aded

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,9 +2,9 @@ import React from 'react';
 //import Image from 'react-bootstrap/Image';
 //import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+//import Button from 'react-bootstrap/Button';
+//import InputGroup from 'react-bootstrap/InputGroup';
+//import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 function Photo(props) {
@@ -12,7 +12,7 @@ function Photo(props) {
       <div style={{display: 'flex', flexFlow: "row wrap", justifyContent: "center"}}>
         {props.photos.map((photo) => {
             return (
-              <Card style={{ width: '150px', marginTop: "10px", marginRight: "10px" }}>
+              <Card key={photo.id} style={{ width: '150px', marginTop: "10px", marginRight: "10px" }}>
   <Card.Img variant="top" src={photo.thumbnailUrl} />
   <Card.Body style={{display: 'flex', flexFlow: "column wrap", justifyContent: "space-between"}}>
     <Card.Title>{photo.title}</Card.Title>
@@ -20,7 +20,7 @@ function Photo(props) {
       <Dropdown.Toggle variant="secondary" id="firstDropdownToggle">
 
         Mark tag
-      
+
       </Dropdown.Toggle>
         <Dropdown.Menu>
         <Dropdown.Item href="#">Awesome!</Dropdown.Item>
